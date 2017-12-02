60c27623415b7f18be807ac5b90cf216e08926ef
jalcantarab <jorgeakairos@gmail.com>
2017-12-01 20:42:27 -0800

    change auth method, added further comments, updated html with valid tags

--- README.md
+++ README.md
@@ -1,37 +1,40 @@
 # isa-bot-sdk
-Example of use of the **Inbenta SDK**, and **webhooks** defined called from Inbenta's chatbot management tool. The Webhooks are programmed using Google Apps Script (similar to JS). 
+Example of use of the **Inbenta SDK**, and **webhooks** defined called from Inbenta's chatbot management tool. The Webhooks are programmed using Google Apps Script (similar to JS).
 * isa-bot-sdk.html - Empty page with the calls to the Inbenta Chatbot SDK and the below css/js files.
 * isa-bot-sdk.js - JavaScript that defines the configuration of the chatbot and launches the methods to build it on the page.
-* isa-bot-sdk.css - CSS that overrides the Inbenta template with custom styling. 
+* isa-bot-sdk.css - CSS that overrides the Inbenta template with custom styling.
 * isa-bot-sdk-CustomMethods.js - Custom methods for added functionality. So far:
 * * getMessageHistory
 * * sendMessageHistory
 
-For the simplest possible example, please go to **[This Gist](https://gist.github.com/jalcantarab/e56f059455468094509c54fc951329dc)**. 
+For the simplest possible example, please go to **[This Gist](https://gist.github.com/jalcantarab/e56f059455468094509c54fc951329dc)**.
 
 For API examples, please check **[This Other One](https://gist.github.com/jalcantarab/0550eb938456c5d91cb28e2db4784eb1)**
 
 # Chatbot SDK Example - Custom initialization
-Simple example of a functioning chatbot SDK User Interface 
-This example is meant to show how to: 
-* Custom configuration of the SDK build, 
-* Customize the labels to align with your desired language and personality, 
+Simple example of a functioning chatbot SDK User Interface
+This example is meant to show how to:
+* Custom configuration of the SDK build,
+* Customize the labels to align with your desired language and personality,
 * Customize some CSS to fit your colors and icons,
-* How to use the /auth API method to retrieve the API Token. 
+* How to use the /auth API method to retrieve the API Token.
 * Create custom JS methods that interact with the Chatbot
 
-## Set up 
-Either: 
-* Publish the at least the HTML and JS somewhere, and call the JS from the HTML. 
+## Set up
+Either:
+* Publish the at least the HTML and JS somewhere, and call the JS from the HTML.
 * Copy the call to the SDK from the HTML to your page's HTML and the JavaScript
   `<script type="text/javascript" src="https://sdk.inbenta.io/chatbot/1/inbenta-chatbot-sdk.js"></script>`
-  and add the JavaScript file `isa-bot-sdk.js` either inside the HTML (inside `<script>` tags) or in a file then called from the page. 
-  
+  and add the JavaScript file `isa-bot-sdk.js` either inside the HTML (inside `<script>` tags) or in a file then called from the page.
+
 
 ## Usage
-Replace the Secret <inbenta-secret> and API key <inbenta-key> with your instance's Secret and API key.
+Replace the Secret <inbenta-secret> and API key <inbenta-key> with a method that retrieves the values server-side.
+For development, you can replace the values with your credentials.
 
-## Contributing 
+If you don't have your API Secret and Key, contact you account manager.
+
+## Contributing
 If you have useful new methods, webhooks, or better ways of doing what this sample does, please feel free to...
 
 1. Fork it!
@@ -44,5 +47,5 @@ If you have useful new methods, webhooks, or better ways of doing what this samp
 v0.0.1 Documentation updated and (some) comments added
 v0.1.1 Inclusion of customMethods for callbacks and getTranscripts()
 v0.1.12 First functional version using the createFromAccessToken().
-v0.1.27 First Version in Git. 
+v0.1.27 First Version in Git.
 v0.1.53 Webhooks ready to be included; Authentication corrected; CSS expanded for webhooks; bug fixes
--- isa-bot-sdk.js
+++ isa-bot-sdk.js
@@ -1,67 +1,76 @@
-initSDK();
+// Chatbot SDK configuration.
+// Here you define the labels and settings of your user interface
+// See more at https://apidocs.inbenta.io/chatbot/sdk/configuration
+var InbentaConfiguration = {
+  // Language of the interface. Currently only English has all tags defined
+    lang:'en',
+  // answers object, defines which fields from the content are shown to the user as results
+    "answers":{
+      // answerAttributes marks which field will be used for in-dialog responses
+      "answerAttributes": ["ANSWER_TEXT"],
+      //sideBubbleAttributes marks which for side-bubble responses
+      "sideBubbleAttributes": ["SIDEBUBBLE_TEXT"]
+    },
+  // The Audience level of the users for this chatbot. If your instance doesn't use profiles, use always 0 (or leave empty)
+    usertype: 0,
+    environment: "production",
+  // Customize the hovering widget (launcher)
+    launcher: {
+      // Add a title to display by the icon
+      title:""
+    },
+  // 'labels' Overrides the label values from the SDK template
+    labels: {
+      en: {
+        'yes' : 'Sure',
+        'no' : 'Nope',
+        'generic-error-message' : "I'm still in training, please try again",
+        'enter-question' : 'Ask Me!',
+        'interface-title' : 'I.S.A 0099',
+        'guest-name' : 'You',
+        'help-question' : "Hello!, I'm Inbenta Support Agent v0.1.53, what can I do for you?",
+        'thanks' : 'Thank you!',
+        'rate-content' : 'Did I help you?',
+        'form-message' : 'Please tell me how I can get better at my job',
+        'submit' : 'Send it!'
+      }
+    }
+  };
 
-function initSDK() {
-  // retrieve the secret and key from server-side or do the rest of this process server-side
-  var inbenta_secret="<inbenta_secret>";
-  var inbenta_key="<inbenta_key>";
-  /* authPOST gets the bearer token 
-   * authenticates the SDK build by passing it as a callback
-   * This authentication mechanism is for TESTS ONLY. 
+// Initialize the SDK after the page loads
+window.onload = function() {
+  /*
+   * Authenticates the SDK build by passing it as a callback
+   * This authentication mechanism is for TESTS ONLY.
    * A Production environment should authenticate server-side
-  */ 
-  var inbenta_token = authPOST(inbenta_key, inbenta_secret, initBot);
+  */
+  authenticateCall(initBotSDK);
   // x-inbenta-token, x-inbenta-key added to document.cookies
 }
 
-function initBot(inbenta_token, inbenta_key) {
+function initBotSDK(inbenta_token, inbenta_key) {
   // Create authentication variable from bearer token and key
   var InbentaAuth =  InbentaChatbotSDK.createFromAccessToken(inbenta_token,inbenta_key);
   // Customize the configuration of your chatbot here, learn more at apidocs.inbenta.io/chatbot/sdk
-  var InbentaConfiguration = {
-    // Language of the interface. Currently only English has all tags defined
-      lang:'en',
-    // answers object, defines which fields from the content are shown to the user as results
-      "answers":{
-        // answerAttributes marks which field will be used for in-dialog responses
-        "answerAttributes": ["ANSWER_TEXT"],
-        //sideBubbleAttributes marks which for side-bubble responses
-        "sideBubbleAttributes": ["SIDEBUBBLE_TEXT"]
-      },
-    // The Audience level of the users for this chatbot. If your instance doesn't use profiles, use always 0 (or leave empty)
-      usertype: 0,
-      environment: "production",
-    // Customize the hovering widget (launcher)
-      launcher: {
-        // Add a title to display by the icon
-        title:""
-      },
-    // 'labels' Overrides the label values from the SDK template
-      labels: {
-        en: {
-          'yes' : 'Sure',
-          'no' : 'Nope',
-          'generic-error-message' : "I'm still in training, please try again",
-          'enter-question' : 'Ask Me!',
-          'interface-title' : 'I.S.A 0099',
-          'guest-name' : 'You',
-          'help-question' : "Hello!, I'm Inbenta Support Agent v0.1.53, what can I do for you?",
-          'thanks' : 'Thank you!',
-          'rate-content' : 'Did I help you?',
-          'form-message' : 'Please tell me how I can get better at my job',
-          'submit' : 'Send it!'
-        }
-      }
-    }; 
-  // Build the Chatbot interface using the above configuration and auth variables 
+
+  // Build the Chatbot interface using the above configuration and auth variables
   InbentaChatbotSDK.build(InbentaAuth, InbentaConfiguration);
 }
 
-function authPOST(inbenta_key, inbenta_secret, callback) {
+function authenticateCall(callback) {
   var inbenta_token, token_expiration = "";
+  // Before a live deployment, move the authentication server-side
+  // inbenta_secret server-side
+  // Credentials should not be visible in a front other
+  // than during initial development
+  var inbenta_secret = getSecret().secret;
+  var inbenta_key = getSecret().apiKey;
   // Include the secret on the payload
-  var data = {"secret":inbenta_secret}; 
+  var data = {"secret":inbenta_secret};
   var xhr = new XMLHttpRequest();
-  
+
+  setCookie("x-inbenta-key",inbenta_key);
+
   xhr.open("POST", "https://api.inbenta.io/v1/auth");
   xhr.setRequestHeader("content-type", "application/json");
   xhr.setRequestHeader("x-inbenta-key", inbenta_key);
@@ -78,7 +87,6 @@ function authPOST(inbenta_key, inbenta_secret, callback) {
     return;
   }};
   xhr.send(JSON.stringify(data));
-  setCookie("x-inbenta-key",inbenta_key);
 }
 
 function setCookie(cname, cvalue, exdays) {
